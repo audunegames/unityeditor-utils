eee45db399cee5dfb029a0af4b2e87ff8deb6abb
Danae Dekker <info@danaedekker.com>
2024-02-23 23:30:37 +0100

    Added enum flags drawers; added drawers with prefix labels

--- Editor/Extensions/EditorGUIExtensions.cs
+++ Editor/Extensions/EditorGUIExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
 
 namespace Audune.Utils.UnityEditor.Editor
 {
@@ -120,7 +121,17 @@ namespace Audune.Utils.UnityEditor.Editor
     #endregion
 
     #region Drawing an item popup
-    // Draw a popup containing the items in the list with the specified label
+    // Draw a popup containing the items in the list
+    public static T ItemPopup<T>(Rect rect, List<T> items, Predicate<T> selected, Func<T, GUIContent> displayedItemSelector)
+    {
+      return ItemPopup(rect, null, items, selected, displayedItemSelector);
+    }
+    public static T ItemPopup<T>(Rect rect, List<T> items, T selected, Func<T, GUIContent> displayedItemSelector)
+    {
+      return ItemPopup(rect, null, items, selected, displayedItemSelector);
+    }
+
+    // Draw a popup containing the items in the list with a prefix label
     public static T ItemPopup<T>(Rect rect, GUIContent label, List<T> items, Predicate<T> selected, Func<T, GUIContent> displayedItemSelector)
     {
       var currentIndex = Mathf.Max(0, items.FindIndex(selected));
@@ -131,43 +142,104 @@ namespace Audune.Utils.UnityEditor.Editor
     {
       return ItemPopup(rect, label, items, t => t.Equals(selected), displayedItemSelector);
     }
-
-    // Draw a popup containing the items in the list
-    public static T ItemPopup<T>(Rect rect, List<T> items, Predicate<T> selected, Func<T, GUIContent> displayedItemSelector)
-    {
-      return ItemPopup(rect, null, items, selected, displayedItemSelector);
-    }
-    public static T ItemPopup<T>(Rect rect, List<T> items, T selected, Func<T, GUIContent> displayedItemSelector)
-    {
-      return ItemPopup(rect, null, items, selected, displayedItemSelector);
-    }
     #endregion
 
     #region Drawing a generic menu dropdown
     // Draw a generic menu dropdown
-    public static void GenericMenuDropdown(Rect rect, GUIContent label, GenericMenu menu)
+    public static void GenericMenuDropdown(Rect rect, GUIContent dropdownLabel, GenericMenu menu)
     {
-      if (EditorGUI.DropdownButton(rect, label, FocusType.Keyboard))
+      if (EditorGUI.DropdownButton(rect, dropdownLabel, FocusType.Keyboard))
         menu.DropDown(rect);
     }
 
+    // Draw a generic menu dropdown with a prefix label
+    public static void GenericMenuDropdown(Rect rect, GUIContent label, GUIContent dropdownLabel, GenericMenu menu)
+    {
+      if (label != null)
+        rect = EditorGUI.PrefixLabel(rect, label);
+      GenericMenuDropdown(rect, dropdownLabel, menu);
+    }
+
     // Return the height of a generic menu dropdown
-    public static float GetGenericMenuDropdownHeight(GUIContent label, GenericMenu menu)
+    public static float GetGenericMenuDropdownHeight(GenericMenu menu)
     {
       return EditorGUIUtility.singleLineHeight;
     }
     #endregion
 
+    #region Drawing an enum flags dropdown
+    // Draw an enum flags dropdown
+    public static void EnumFlagsDropdown<TEnum>(Rect rect, SerializedProperty serializedProperty) where TEnum : Enum
+    {
+      GenericMenuDropdown(rect, new GUIContent(ObjectNames.NicifyVariableName(((TEnum)Enum.ToObject(typeof(TEnum), serializedProperty.enumValueFlag)).ToString())), CreateEnumFlagsMenu<TEnum>(serializedProperty));
+    }
+
+    // Draw an enum flags dropdown with a prefix label
+    public static void EnumFlagsDropdown<TEnum>(Rect rect, GUIContent label, SerializedProperty serializedProperty) where TEnum : Enum
+    {
+      if (label != null)
+        rect = EditorGUI.PrefixLabel(rect, label);
+      EnumFlagsDropdown<TEnum>(rect, serializedProperty);
+    }
+
+    // Return the height an enum flags dropdown
+    public static float GetEnumFlagsDropdownHeight<TEnum>(SerializedProperty serializedProperty) where TEnum : Enum
+    {
+      return GetGenericMenuDropdownHeight(CreateEnumFlagsMenu<TEnum>(serializedProperty));
+    }
+
+    // Create an enum flag menu for a property
+    private static GenericMenu CreateEnumFlagsMenu<TEnum>(SerializedProperty serializedProperty) where TEnum : Enum
+    {
+      var menu = new GenericMenu();
+      foreach (var flag in (TEnum[])Enum.GetValues(typeof(TEnum)))
+        menu.AddItem(new GUIContent(ObjectNames.NicifyVariableName(Enum.GetName(typeof(TEnum), flag))), HasEnumFlag(serializedProperty, Convert.ToInt32(flag)), () => SetEnumFlag(serializedProperty, Convert.ToInt32(flag)));
+      return menu;
+    }
+
+    // Return if a property has an enum flag
+    private static bool HasEnumFlag(SerializedProperty serializedProperty, int flag)
+    {
+      if (flag == 0)
+        return serializedProperty.enumValueFlag == 0;
+      else
+        return (serializedProperty.enumValueFlag & flag) == flag;
+    }
+
+    // Set the enum flag of a property
+    private static void SetEnumFlag(SerializedProperty serializedProperty, int flag)
+    {
+      serializedProperty.serializedObject.Update();
+
+      if (flag == 0)
+        serializedProperty.enumValueFlag = 0;
+      if ((serializedProperty.enumValueFlag & flag) == flag)
+        serializedProperty.enumValueFlag &= ~flag;
+      else
+        serializedProperty.enumValueFlag |= flag;
+
+      serializedProperty.serializedObject.ApplyModifiedProperties();
+    }
+    #endregion
+
     #region Drawing a search tree view dropdown
     // Draw a search dropdown
-    public static void SearchDropdown<T, TWindow>(Rect rect, GUIContent label, SerializedProperty serializedProperty) where TWindow : SearchWindow<T>
+    public static void SearchDropdown<T, TWindow>(Rect rect, GUIContent dropdownLabel, SerializedProperty serializedProperty) where TWindow : SearchWindow<T>
     {
-      if (EditorGUI.DropdownButton(rect, label, FocusType.Keyboard))
+      if (EditorGUI.DropdownButton(rect, dropdownLabel, FocusType.Keyboard))
         SearchWindow<T>.ShowAsDropDown<TWindow>(rect, serializedProperty);
     }
 
+    // Draw a search dropdown with a prefix label
+    public static void SearchDropdown<T, TWindow>(Rect rect, GUIContent label, GUIContent dropdownLabel, SerializedProperty serializedProperty) where TWindow : SearchWindow<T>
+    {
+      if (label != null)
+        rect = EditorGUI.PrefixLabel(rect, label);
+      SearchDropdown<T, TWindow>(rect, dropdownLabel, serializedProperty);
+    }
+
     // Return the height of a search dropdown
-    public static float GetSearchDropdownHeight(GUIContent label, SerializedProperty serializedProperty)
+    public static float GetSearchDropdownHeight(SerializedProperty serializedProperty)
     {
       return EditorGUIUtility.singleLineHeight;
     }
--- Editor/Extensions/EditorGUILayoutExtensions.cs
+++ Editor/Extensions/EditorGUILayoutExtensions.cs
@@ -69,17 +69,6 @@ namespace Audune.Utils.UnityEditor.Editor
     #endregion
 
     #region Drawing an item popup
-    // Draw a popup containing the items in the list with the specified label
-    public static T ItemPopup<T>(GUIContent label, List<T> items, Predicate<T> selected, Func<T, GUIContent> displayedItemSelector, params GUILayoutOption[] options)
-    {
-      var rect = EditorGUILayout.GetControlRect(label != null, EditorGUIUtility.singleLineHeight, options);
-      return EditorGUIExtensions.ItemPopup(rect, label, items, selected, displayedItemSelector);
-    }
-    public static T ItemPopup<T>(GUIContent label, List<T> items, T selected, Func<T, GUIContent> displayedItemSelector, params GUILayoutOption[] options)
-    {
-      return ItemPopup(label, items, t => t.Equals(selected), displayedItemSelector, options);
-    }
-
     // Draw a popup containing the items in the list
     public static T ItemPopup<T>(List<T> items, Predicate<T> selected, Func<T, GUIContent> displayedItemSelector, params GUILayoutOption[] options)
     {
@@ -89,23 +78,62 @@ namespace Audune.Utils.UnityEditor.Editor
     {
       return ItemPopup(null, items, selected, displayedItemSelector, options);
     }
+
+    // Draw a popup containing the items in the list with a prefix label
+    public static T ItemPopup<T>(GUIContent label, List<T> items, Predicate<T> selected, Func<T, GUIContent> displayedItemSelector, params GUILayoutOption[] options)
+    {
+      var rect = EditorGUILayout.GetControlRect(label != null, EditorGUIUtility.singleLineHeight, options);
+      return EditorGUIExtensions.ItemPopup(rect, label, items, selected, displayedItemSelector);
+    }
+    public static T ItemPopup<T>(GUIContent dropdownLabel, List<T> items, T selected, Func<T, GUIContent> displayedItemSelector, params GUILayoutOption[] options)
+    {
+      return ItemPopup(dropdownLabel, items, t => t.Equals(selected), displayedItemSelector, options);
+    }
     #endregion
 
     #region Drawing a generic menu dropdown
     // Draw a generic menu dropdown
-    public static void GenericMenuDropdown(GUIContent label, GenericMenu menu, params GUILayoutOption[] options)
+    public static void GenericMenuDropdown(GUIContent dropdownLabel, GenericMenu menu, params GUILayoutOption[] options)
+    {
+      var rect = EditorGUILayout.GetControlRect(false, EditorGUIUtility.singleLineHeight, options);
+      EditorGUIExtensions.GenericMenuDropdown(rect, dropdownLabel, menu);
+    }
+
+    // Draw a generic menu dropdown with a prefix label
+    public static void GenericMenuDropdown(GUIContent label, GUIContent dropdownLabel, GenericMenu menu, params GUILayoutOption[] options)
     {
       var rect = EditorGUILayout.GetControlRect(label != null, EditorGUIUtility.singleLineHeight, options);
-      EditorGUIExtensions.GenericMenuDropdown(rect, label, menu);
+      EditorGUIExtensions.GenericMenuDropdown(rect, label, dropdownLabel, menu);
+    }
+    #endregion
+
+    #region Drawing an enum flags dropdown
+    // Draw an enum flags dropdown
+    public static void EnumFlagsDropdown<TEnum>(SerializedProperty serializedProperty, params GUILayoutOption[] options) where TEnum : Enum
+    {
+      var rect = EditorGUILayout.GetControlRect(false, EditorGUIUtility.singleLineHeight, options);
+      EditorGUIExtensions.EnumFlagsDropdown<TEnum>(rect, serializedProperty);
+    }
+    // Draw an enum flags dropdown with a prefix label
+    public static void EnumFlagsDropdown<TEnum>(GUIContent label, SerializedProperty serializedProperty, params GUILayoutOption[] options) where TEnum : Enum
+    {
+      var rect = EditorGUILayout.GetControlRect(label != null, EditorGUIUtility.singleLineHeight, options);
+      EditorGUIExtensions.EnumFlagsDropdown<TEnum>(rect, label, serializedProperty);
     }
     #endregion
 
     #region Drawing a search tree view dropdown
     // Draw a search dropdown
-    public static void SearchDropdown<T, TWindow>(GUIContent label, SerializedProperty serializedProperty, params GUILayoutOption[] options) where TWindow : SearchWindow<T>
+    public static void SearchDropdown<T, TWindow>(GUIContent dropdownLabel, SerializedProperty serializedProperty, params GUILayoutOption[] options) where TWindow : SearchWindow<T>
+    {
+      var rect = EditorGUILayout.GetControlRect(false, EditorGUIUtility.singleLineHeight, options);
+      EditorGUIExtensions.SearchDropdown<T, TWindow>(rect, dropdownLabel, serializedProperty);
+    }
+    // Draw a search dropdown with a prefix label
+    public static void SearchDropdown<T, TWindow>(GUIContent label, GUIContent dropdownLabel, SerializedProperty serializedProperty, params GUILayoutOption[] options) where TWindow : SearchWindow<T>
     {
       var rect = EditorGUILayout.GetControlRect(label != null, EditorGUIUtility.singleLineHeight, options);
-      EditorGUIExtensions.SearchDropdown<T, TWindow>(rect, label, serializedProperty);
+      EditorGUIExtensions.SearchDropdown<T, TWindow>(rect, label, dropdownLabel, serializedProperty);
     }
     #endregion
   }
