7ea515cad693689c8945d0645f59e9885f9960be
Danae Dekker <info@danaedekker.com>
2024-02-29 20:06:05 +0100

    Removed unnecessary imports

--- Editor/Extensions/EditorGUIExtensions.cs
+++ Editor/Extensions/EditorGUIExtensions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
-using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
 
 namespace Audune.Utils.UnityEditor.Editor
 {
